526c01a8de81f5432bb0a8c7c2925e0d796fb137
Fernanda Barbalho <fl.barbalho@gmail.com>
2024-09-29 19:13:34 +0000

    Atividade 1: Rotas

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Desenvolvimento Web: Servidor - API's com Flask
-Código produzidos durante o quarto semestre de Análise e Desenvolvimento de Sistemas (IFSP), referente à matéria de Desenvolvimento Web: Servidor.
+Códigos produzidos durante o quarto semestre de Análise e Desenvolvimento de Sistemas (IFSP), referente à matéria de Desenvolvimento Web: Servidor.
+
+## Implementação de Rotas - 29/09/2024
+https://fernandabarbalho.pythonanywhere.com/
+https://fernandabarbalho.pythonanywhere.com/user/Fabio Teixeira
+https://fernandabarbalho.pythonanywhere.com/contextorequisicao
+https://fernandabarbalho.pythonanywhere.com/codigostatusdiferente
+https://fernandabarbalho.pythonanywhere.com/objetoresposta
+https://fernandabarbalho.pythonanywhere.com/redirecionamento
+https://fernandabarbalho.pythonanywhere.com/abortar
--- hello.py
+++ hello.py
@@ -1,11 +1,35 @@
-# A very simple Flask Hello World app for you to get started with...
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
+from flask import Flask, request, make_response, redirect, abort
 
+app = Flask(__name__)
+
+@app.route('/')
 def hello_world():
-    return '<p>Alterações por meio do GitHub</p><table><tr><td><b>Aluna:</b></td><td>Fernanda Lopes Barbalho</td></tr><tr><td><b>Prontuário:</b></td><td>PT302525xX</td></tr></table>'
+    return '<h1>Hello World!</h1>'
 
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
+
+@app.route('/contextorequisicao')
+def contexto_requisicao():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
+
+@app.route('/codigostatusdiferente')
+def codigo_status_diferente():
+    return '<p>Bad request</p>', 400
+
+@app.route('/objetoresposta')
+def objeto_resposta():
+   response = make_response('<h1>This document carries a cookie!</h1>')
+   response.set_cookie('answer', '42')
+   return response
+
+@app.route('/redirecionamento')
+def redirecionamento():
+   location = 'https://ptb.ifsp.edu.br/'
+   return redirect(location)
+
+@app.route('/abortar')
+def abortar():
+   return abort(404)
