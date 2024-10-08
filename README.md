3d5c6a2a5f2afb52aa176014216fee842b6c9b4e
Fernanda Barbalho <fl.barbalho@gmail.com>
2024-10-08 23:53:40 +0000

    Atividade 2: Rotas 2

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 # Desenvolvimento Web: Servidor - API's com Flask
 Códigos produzidos durante o quarto semestre de Análise e Desenvolvimento de Sistemas (IFSP), referente à matéria de Desenvolvimento Web: Servidor.
 
-## Implementação de Rotas - 29/09/2024
+## Implementação de Rotas 2 - 08/10/2024
 - https://fernandabarbalho.pythonanywhere.com/
-- https://fernandabarbalho.pythonanywhere.com/user/Fabio%20Teixeira
+- https://fernandabarbalho.pythonanywhere.com/user/Fabio%20Teixeira/PT23820X/IFSP
 - https://fernandabarbalho.pythonanywhere.com/contextorequisicao
-- https://fernandabarbalho.pythonanywhere.com/codigostatusdiferente
-- https://fernandabarbalho.pythonanywhere.com/objetoresposta
-- https://fernandabarbalho.pythonanywhere.com/redirecionamento
-- https://fernandabarbalho.pythonanywhere.com/abortar
--- hello.py
+++ hello.py
@@ -1,35 +1,18 @@
-from flask import Flask, request, make_response, redirect, abort
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World!</h1>'
+def home():
+    return '<h1>Avaliação contínua: Aula 030</h1><ul><li><a href="/">Home</a></li><li><a href="/user/Fabio%20Teixeira/PT23820X/IFSP">Identificação</a></li><li><a href="/contextorequisicao">Contexto da requisição</a></li></ul>'
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+@app.route('/user/<name>/<register>/<institution>')
+def user(name, register, institution):
+    return '<h1>Avaliação contínua: Aula 030</h1><h2>Aluno: {}</h2><h2>Prontuário: {}</h2><h2>Instituição: {}</h2><a href="/">Voltar</a>'.format(name, register, institution)
 
 @app.route('/contextorequisicao')
 def contexto_requisicao():
     user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is {}</p>'.format(user_agent)
-
-@app.route('/codigostatusdiferente')
-def codigo_status_diferente():
-    return '<p>Bad request</p>', 400
-
-@app.route('/objetoresposta')
-def objeto_resposta():
-   response = make_response('<h1>This document carries a cookie!</h1>')
-   response.set_cookie('answer', '42')
-   return response
-
-@app.route('/redirecionamento')
-def redirecionamento():
-   location = 'https://ptb.ifsp.edu.br/'
-   return redirect(location)
-
-@app.route('/abortar')
-def abortar():
-   return abort(404)
+    ip = request.remote_addr
+    host = request.headers.get('Host')
+    return '<h1>Avaliação contínua: Aula 030</h1><h2>Seu navegador é: {}</h2><h2>O IP do cumputador remoto é: {}</h2><h2>O host da aplicação é: {}</h2><a href="/">Voltar</a>'.format(user_agent, ip, host)
