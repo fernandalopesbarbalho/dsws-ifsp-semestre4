3a8788887fc8af6821abd1103c2d0b8c26f0c941
Fernanda Barbalho <fl.barbalho@gmail.com>
2024-10-14 21:54:20 +0000

    Atividade 3: Templates

--- hello.py
+++ hello.py
@@ -1,18 +1,24 @@
-from flask import Flask, request
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
-def home():
-    return '<h1>Avaliação contínua: Aula 030</h1><ul><li><a href="/">Home</a></li><li><a href="/user/Fabio%20Teixeira/PT23820X/IFSP">Identificação</a></li><li><a href="/contextorequisicao">Contexto da requisição</a></li></ul>'
+def index():
+    return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('/user/<name>/<register>/<institution>')
-def user(name, register, institution):
-    return '<h1>Avaliação contínua: Aula 030</h1><h2>Aluno: {}</h2><h2>Prontuário: {}</h2><h2>Instituição: {}</h2><a href="/">Voltar</a>'.format(name, register, institution)
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
-@app.route('/contextorequisicao')
-def contexto_requisicao():
-    user_agent = request.headers.get('User-Agent')
-    ip = request.remote_addr
-    host = request.headers.get('Host')
-    return '<h1>Avaliação contínua: Aula 030</h1><h2>Seu navegador é: {}</h2><h2>O IP do cumputador remoto é: {}</h2><h2>O host da aplicação é: {}</h2><a href="/">Voltar</a>'.format(user_agent, ip, host)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
