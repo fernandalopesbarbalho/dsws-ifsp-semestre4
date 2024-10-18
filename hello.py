5ce10bd53989ed11456c3b583458dbbff1eec331
Fernanda Barbalho <fl.barbalho@gmail.com>
2024-10-18 01:43:44 +0000

    Atividade 4: Templates 2

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -11,9 +11,16 @@ moment = Moment(app)
 def index():
     return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/user/<name>/<register>/<institution>')
+def user(name, register, institution):
+    return render_template('user.html', name=name, register=register, institution=institution)
+
+@app.route('/contextorequisicao/<name>')
+def contexto_requisicao(name):
+    user_agent = request.headers.get('User-Agent')
+    ip = request.remote_addr
+    host = request.headers.get('Host')
+    return render_template('contexto.html', name=name, user_agent=user_agent, ip=ip, host=host)
 
 @app.errorhandler(404)
 def page_not_found(e):
