0cfed935a6ec8b9297cf6e224094d9f7c39617c2
Fernanda Barbalho <fl.barbalho@gmail.com>
2024-11-03 18:45:31 +0000

    Atividade 6: Formulários 2

--- hello.py
+++ hello.py
@@ -2,28 +2,56 @@ from flask import Flask, render_template, request, session, redirect, url_for, f
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 app.config['SECRET_KEY'] = 'fhoeo67g293bsdt62h#5555&gididwei'
 
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
+class UserInfoForm(FlaskForm):
+    name = StringField('Informe o seu nome', validators=[DataRequired()])
+    surname = StringField('Informe o seu sobrenome', validators=[DataRequired()])
+    institution = StringField('Informe a sua Instituição de ensino', validators=[DataRequired()])
+    discipline = SelectField('Informe a sua disciplina', choices=[
+        ('DSWA5', 'DSWA5'),
+        ('DWBA4', 'DWBA4'),
+        ('Gestão de projetos', 'Gestão de projetos')
+    ])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = UserInfoForm()
     if form.validate_on_submit():
         old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data
+        new_name = form.name.data
+
+        if old_name and old_name != new_name:
+            flash('Você alterou o seu nome!')
+
+        session['name'] = new_name
+        session['surname'] = form.surname.data
+        session['institution'] = form.institution.data
+        session['discipline'] = form.discipline.data
+        session['ip'] = request.remote_addr
+        session['host'] = request.host
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+
+    return render_template(
+        'index.html',
+        form=form,
+        name=session.get('name'),
+        surname=session.get('surname'),
+        institution=session.get('institution'),
+        discipline=session.get('discipline'),
+        ip=session.get('ip', 'None'),
+        host=session.get('host', 'None'),
+        current_time=datetime.utcnow()
+    )
 
 @app.route('/user/<name>/<register>/<institution>')
 def user(name, register, institution):
