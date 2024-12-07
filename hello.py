a3fbbddae712cdcfa5d44034868ae14a02514284
Fernanda Barbalho <fl.barbalho@gmail.com>
2024-12-07 01:08:22 +0000

    Atividade 11: E-mail 2

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -75,7 +75,8 @@ def send_simple_message(to, subject, newUser):
 
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
+    name = StringField('Qual é o seu nome?', validators=[DataRequired()])
+    email_send = BooleanField('Deseja enviar e-mail para flaskaulasweb@zohomail.com?')
     submit = SubmitField('Submit')
 
 
@@ -105,22 +106,19 @@ def index():
             db.session.commit()
             session['known'] = False
 
-            print('Verificando variáveis de ambiente: Server log do PythonAnyWhere', flush=True)
-            print('FLASKY_ADMIN: ' + str(app.config['FLASKY_ADMIN']), flush=True)
-            print('URL: ' + str(app.config['API_URL']), flush=True)
-            print('api: ' + str(app.config['API_KEY']), flush=True)
-            print('from: ' + str(app.config['API_FROM']), flush=True)
-            print('to: ' + str([app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"]), flush=True)
-            print('subject: ' + str(app.config['FLASKY_MAIL_SUBJECT_PREFIX']), flush=True)
-            print('text: ' + "Novo usuário cadastrado: " + form.name.data, flush=True)
-
-            if app.config['FLASKY_ADMIN']:
-                print('Enviando mensagem...', flush=True)
-                send_simple_message([app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"], 'Novo usuário', form.name.data)
-                print('Mensagem enviada...', flush=True)
+            if form.email_send.data:
+                if app.config['FLASKY_ADMIN']:
+                    send_simple_message(
+                        [app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"],
+                        'Novo usuário',
+                        form.name.data
+                    )
         else:
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
+
+    users = User.query.all()
+
     return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False))
+                           known=session.get('known', False), users=users)
